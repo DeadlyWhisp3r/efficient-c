4dbcc9c202e4c612dc504ffbfeaf5711b61c79ce
Casper Vikstrom <caspervikstrom@gmail.com>
2023-11-23 10:49:17 +0100

    fixed bug of reading size on c[i]. also finished testing git. Now the working code with the commented malloc is running. Just uncomment and comment the for loop and run 'valgrind --track-origins=yes ./a.out < indata.txt'

--- lab3.c
+++ lab3.c
@@ -24,12 +24,11 @@ int init (simplex_t *s, int m,  int n, double **a, double *b, double *c, double 
     //*s = (simplex_t) {s->m=m,s->n=n,s->var=var,s->a=a,s->b=b,s->x=x,s->c=c,s->y=y}; // assign each attribute
     *s=(simplex_t){m,n,var,a,b,x,c,y};
     if (s->var == NULL){
-        //s->var =  calloc(m+n+1, sizeof(int));
-        s->var =  malloc((m+n+1)* sizeof(int));
-        // for (i = 0; i < m+n; i = i + 1){
-        //     s->var[i] = i;
-        // }
-        //testing again
+        s->var =  calloc(m+n+1, sizeof(int));
+        // s->var =  malloc((m+n+1)* sizeof(int));
+        for (i = 0; i < m+n; i = i + 1){
+            s->var[i] = i;
+        }
     }
     for (k = 0, i = 1; i < m; i = i + 1){
         if (s->b[i] < s->b[k]){
@@ -284,7 +283,7 @@ int main(int agrc, char** argv)
         scanf("%lf", &b[i]);
     }
     printf("max z = ");
-    for (int i=0 ; i<m ; i+=1){
+    for (int i=0 ; i<n ; i+=1){
         printf("%10.3lfx%d", c[i],i);
         if (i<n-1) printf(" + ");
     }
